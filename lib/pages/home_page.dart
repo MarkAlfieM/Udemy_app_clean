9432b729fffa01fc16f234889326b844c2450e6b
MarkAlfieM <136426824+MarkAlfieM@users.noreply.github.com>
2025-02-05 12:06:52 +0800

    S4L41
    Last Addition: Dropdown Menu Button with Items

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  late double _deviceHeight, _deviceWidth;
+
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+        height: _deviceHeight,
+        width: _deviceWidth,
+        padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.05),
+        child: _destinationDropdownWidget(),
+      )),
+    );
+  }
+
+  Widget _pageTitle() {
+    return const Text(
+      "#GoMoon",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 70,
+        fontWeight: FontWeight.w800,
+      ),
+    );
+  }
+
+  Widget _astroImageWidget() {
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+        fit: BoxFit.fill,
+        image: AssetImage("assets/images/astro_moon.png"),
+      )),
+    );
+  }
+
+  Widget _destinationDropdownWidget() {
+    List<DropdownMenuItem<String>> _items = [
+      'James Webb Station',
+      'Prenuere Station',
+    ].map(
+      (e) {
+        return DropdownMenuItem(
+          child: Text(e),
+          value: e,
+        );
+      },
+    ).toList();
+    return Container(
+      child: DropdownButton(
+        onChanged: (_) {},
+        items: _items,
+      ),
+    );
+  }
+}
